5dac05987791c8a2201e2b03d91da82095a4ac7a
Babarinde Johnson <102429480+Babarinde1@users.noreply.github.com>
2022-08-18 22:59:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function w3_open() {
+  document.getElementById("button").style.display ="none";
+  document.getElementById("button2").style.display ="none";
+  document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("button").style.display ="block";
+    document.getElementById("button2").style.display ="block";
+    document.getElementById("mySidebar").style.display = "none";
+  }
